a6206eb971bdf9f3d9aa69431cddd462017c2b95
Osiris <noirozr@icloud.com>
2015-10-04 16:30:35 +0800

    dynamic URLS

--- my_blog/urls.py
+++ my_blog/urls.py
@@ -21,7 +21,7 @@ urlpatterns = [
 	#url(r'^$', 'my_blog.views.home', name = 'home'),
 	#url(r'^blog/', include('blog.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'article.views.home'),
-    #url(r'^(?P<my_args>\d+)/$', 'article.views.detail', name = 'detail'),
+    url(r'^$', 'article.views.home', name = 'home'),
+    url(r'^(?P<id>\d+)/$', 'article.views.detail', name = 'detail'),
     #url(r'^test/$', 'article.views.test'),
 ]
